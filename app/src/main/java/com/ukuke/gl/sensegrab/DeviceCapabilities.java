6556d342e3a10280fabce3c04e723611245ebfb7
Gildo <andgildo@gmail.com>
2015-01-20 00:51:08 +0100

    Working Device Capabilities

--- app/src/main/java/com/ukuke/gl/sensegrab/DeviceCapabilities.java
+++ app/src/main/java/com/ukuke/gl/sensegrab/DeviceCapabilities.java
@@ -8,10 +8,37 @@ import android.view.MenuItem;
 import android.hardware.SensorManager;
 import android.content.Context;
 import android.hardware.Sensor;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DeviceCapabilities extends ActionBarActivity {
 
     private SensorManager mSensorManager;
+    public List<ServiceComponent> serviceComponentList = new ArrayList<ServiceComponent>();
+
+    ListView list;
+    String[] web = {
+            "Google Plus",
+            "Twitter",
+            "Windows",
+            "Bing",
+            "Itunes",
+            "Wordpress",
+            "Drupal"
+    } ;
+    Integer[] imageId = {
+            R.drawable.ic_check_grey600_24dp,
+            R.drawable.ic_close_grey600_24dp
+    };
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,68 +46,11 @@ public class DeviceCapabilities extends ActionBarActivity {
         setContentView(R.layout.activity_device_capabilities);
         Log.i("DeviceCapabilities", "Configure your app!");
 
-        // Check Sensors
-        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        // List<Sensor> deviceSensors = mSensorManager.getSensorList(Sensor.TYPE_ALL);
+        populateServiceComponentList();
 
-        if (mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD) != null){
-            // Success! There's a magnetometer.
-            Log.i("DeviceCapabilities", "YES TYPE_MAGNETIC_FIELD!");
-        }
-        else {
-            Log.i("DeviceCapabilities", "NO TYPE_MAGNETIC_FIELD!");
-        }
+        populateListView();
 
-        if (mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) != null){
-            // Success! There's a magnetometer.
-            Log.i("DeviceCapabilities", "YES TYPE_ACCELEROMETER!");
-        }
-        else {
-            Log.i("DeviceCapabilities", "NO TYPE_ACCELEROMETER!");
-        }
-
-        if (mSensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE) != null){
-            // Success! There's a magnetometer.
-            Log.i("DeviceCapabilities", "YES TYPE_AMBIENT_TEMPERATURE!");
-        }
-        else {
-            Log.i("DeviceCapabilities", "NO TYPE_AMBIENT_TEMPERATURE!");
-        }
-
-
-        if (mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE) != null){
-            // Success! There's a magnetometer.
-            Log.i("DeviceCapabilities", "YES TYPE_GYROSCOPE!");
-        }
-        else {
-            Log.i("DeviceCapabilities", "NO TYPE_GYROSCOPE!");
-        }
-
-        if (mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT) != null){
-            // Success! There's a magnetometer.
-            Log.i("DeviceCapabilities", "YES TYPE_LIGHT!");
-        }
-        else {
-            Log.i("DeviceCapabilities", "NO TYPE_LIGHT!");
-        }
-
-        if (mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY) != null){
-            // Success! There's a magnetometer.
-            Log.i("DeviceCapabilities", "YES TYPE_PROXIMITY!");
-        }
-        else {
-            Log.i("DeviceCapabilities", "NO TYPE_PROXIMITY!");
-        }
-
-        if (mSensorManager.getDefaultSensor(Sensor.TYPE_PRESSURE) != null){
-            // Success! There's a magnetometer.
-            Log.i("DeviceCapabilities", "YES TYPE_PRESSURE!");
-        }
-        else {
-            Log.i("DeviceCapabilities", "NO TYPE_PRESSURE!");
-        }
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -102,5 +72,116 @@ public class DeviceCapabilities extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void populateListView() {
+        ArrayAdapter<ServiceComponent> adapter = new MyListAdapter();
+        ListView list = (ListView) findViewById(R.id.listViewDeviceCapabilities);
+        list.setAdapter(adapter);
+    }
+
+    private int populateServiceComponentList() {
+        // Check Sensors
+        int numServices = 0;
+
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+
+        if (mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD) != null){
+            Log.i("DeviceCapabilities", "YES TYPE_MAGNETIC_FIELD!");
+            serviceComponentList.add(new ServiceComponent("Magnetic Field", true));
+            numServices++;
+        }
+        else {
+            Log.i("DeviceCapabilities", "NO TYPE_MAGNETIC_FIELD!");
+            serviceComponentList.add(new ServiceComponent("Magnetic Field", false));
+        }
+
+        if (mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) != null){
+            Log.i("DeviceCapabilities", "YES TYPE_ACCELEROMETER!");
+            serviceComponentList.add(new ServiceComponent("Accelerometer", true));
+            numServices++;
+        }
+        else {
+            Log.i("DeviceCapabilities", "NO TYPE_ACCELEROMETER!");
+            serviceComponentList.add(new ServiceComponent("Accelerometer", false));
+        }
+
+        if (mSensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE) != null){
+            Log.i("DeviceCapabilities", "YES TYPE_AMBIENT_TEMPERATURE!");
+            serviceComponentList.add(new ServiceComponent("Temperature", true));
+            numServices++;
+        }
+        else {
+            Log.i("DeviceCapabilities", "NO TYPE_AMBIENT_TEMPERATURE!");
+            serviceComponentList.add(new ServiceComponent("Temperature", false));
+        }
+
+
+        if (mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE) != null){
+            Log.i("DeviceCapabilities", "YES TYPE_GYROSCOPE!");
+            serviceComponentList.add(new ServiceComponent("Gyroscope", true));
+            numServices++;
+        }
+        else {
+            Log.i("DeviceCapabilities", "NO TYPE_GYROSCOPE!");
+            serviceComponentList.add(new ServiceComponent("Gyroscope", false));
+        }
+
+        if (mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT) != null){
+            Log.i("DeviceCapabilities", "YES TYPE_LIGHT!");
+            serviceComponentList.add(new ServiceComponent("Light Sensor", true));
+            numServices++;
+        }
+        else {
+            Log.i("DeviceCapabilities", "NO TYPE_LIGHT!");
+            serviceComponentList.add(new ServiceComponent("Light Sensor", true));
+        }
+
+        if (mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY) != null){
+            Log.i("DeviceCapabilities", "YES TYPE_PROXIMITY!");
+            serviceComponentList.add(new ServiceComponent("Proximity Sensor", true));
+            numServices++;
+        }
+        else {
+            Log.i("DeviceCapabilities", "NO TYPE_PROXIMITY!");
+            serviceComponentList.add(new ServiceComponent("Proximity Sensor", false));
+        }
+
+        if (mSensorManager.getDefaultSensor(Sensor.TYPE_PRESSURE) != null){
+            Log.i("DeviceCapabilities", "YES TYPE_PRESSURE!");
+            serviceComponentList.add(new ServiceComponent("Pressure Sensor", true));
+            numServices++;
+        }
+        else {
+            Log.i("DeviceCapabilities", "NO TYPE_PRESSURE!");
+            serviceComponentList.add(new ServiceComponent("Pressure Sensor", false));
+        }
+
+        return numServices;
+    }
+
+    private class MyListAdapter extends ArrayAdapter<ServiceComponent> {
+        public MyListAdapter() {
+            super(DeviceCapabilities.this, R.layout.item_view, serviceComponentList);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View itemView = convertView;
+            if (itemView == null) {
+                itemView = getLayoutInflater().inflate(R.layout.item_view, parent, false);
+            }
+
+            ServiceComponent currentServiceComponent = serviceComponentList.get(position);
+
+            ImageView  imageView = (ImageView) itemView.findViewById(R.id.item_imageView);
+            imageView.setImageResource(currentServiceComponent.imageID);
+
+            TextView myText = (TextView) itemView.findViewById(R.id.item_textView);
+            myText.setText(currentServiceComponent.dysplayName);
+
+            return itemView;
+            //return super.getView(position, convertView, parent);
+        }
     }
 }
