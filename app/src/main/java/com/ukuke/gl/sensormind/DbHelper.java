5f1c3d635f7a3e07c67cec10809dae9ffd67abd2
leov85 <leonardo.vivarelli85@gmail.com>
2015-01-23 01:40:30 +0100

    Aggiunta DbHelper.class

---
+++ app/src/main/java/com/ukuke/gl/sensormind/DbHelper.java
@@ -0,0 +1,270 @@
+package com.ukuke.gl.sensormind;
+
+import android.content.Context;
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.database.DatabaseUtils;
+import android.database.Cursor;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.ArrayList;
+
+/**
+ *   Created by Leonardo on 21/01/2015.
+ */
+public class DbHelper extends SQLiteOpenHelper {
+
+    // DB name
+    public static final String DB_name = "DB_Sensormind";
+
+    // Tables
+    //public static final String Samp_type_table = "Samp_type";
+    public static final String Samp_conf_table = "Samp_conf";
+
+    // Sampling types columns
+    //public static final String Samp_type_id = "id"; //int
+    //public static final String Samp_type_name = "type"; //string
+    //public static final String Samp_type_date = "created"; //datetime
+
+    // Sampling configurations columns
+    public static final String Samp_conf_id = "id"; //int
+    public static final String Samp_conf_name ="name"; //string
+    public static final String Samp_conf_type ="type"; //string
+    public static final String Samp_conf_time ="time"; //int, sampling time
+    public static final String Samp_conf_time_unit ="unit"; //int, 0 for sec, 1 for min
+    public static final String Samp_conf_window ="window"; //int window for streaming sensors, must be less than sampling time
+    public static final String Samp_conf_gps ="gps"; //int, not boolean in SQLite, 0 is false, 1 is true
+    public static final String Samp_conf_date = "created";
+
+    // String for creating tables
+    //private static final String Create_Type_Table =
+    //        "create table "+Samp_type_table+"("+Samp_type_id+" integer primary key autoincrement,"+
+    //                Samp_type_name+" text,"+Samp_type_date+" datetime"+")";
+
+    private static final String Create_Conf_Table =
+            "create table "+Samp_conf_table+"("+Samp_conf_id+" integer primary key autoincrement,"+
+                    Samp_conf_name+" text not null,"+Samp_conf_type+" text not null,"+
+                    Samp_conf_time+" integer not null,"+Samp_conf_time_unit+" integer not null,"+
+                    Samp_conf_window+" integer,"+Samp_conf_gps+" integer not null,"+
+                    Samp_conf_date+" datetime"+")";
+
+    public DbHelper(Context context) {
+        super(context, DB_name, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+        //db.execSQL(Create_Type_Table);
+        db.execSQL(Create_Conf_Table);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // delete tables
+        //db.execSQL("drop table if exists"+Samp_type_table);
+        db.execSQL("drop table if exists"+Samp_conf_table);
+        // create new tables
+        onCreate(db);
+    }
+
+
+    // Adapter methods
+
+    //NEW CONFIGURATION
+    public boolean newConfiguration (String name, String type, int time, String unit, int window, boolean gps) {
+        //TODO on upper level: check if window is greater than sampling time for streaming sensors
+        // check if values are correct
+        if (time>0 & (unit.equals("sec") | unit.equals("min"))){
+
+            int time_unit = covertTimeUnit(unit);
+            int intGPS = covertGps(gps);
+
+            SQLiteDatabase db = this.getWritableDatabase(); //open database
+            ContentValues values = new ContentValues();
+
+            values.put(Samp_conf_name, name);
+            values.put(Samp_conf_type, type);
+            values.put(Samp_conf_time, time);
+            values.put(Samp_conf_time_unit, time_unit);
+            values.put(Samp_conf_window, window);
+            values.put(Samp_conf_gps, intGPS);
+            values.put(Samp_conf_date, getDateTime());
+
+            db.insert(Samp_conf_table, null, values);
+            db.close();
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public Cursor getConfigurationById(int id){
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor res = db.rawQuery( "select * from "+Samp_conf_table+" where id= "+id+"", null );
+        db.close();
+        return res;
+    }
+
+    // UPDATE BY ID
+    public boolean updateConfigurationById(int id, String name, String type, int time, String unit, int window, boolean gps) {
+        //TODO on upper level: check if window is greater than sampling time for streaming sensors
+        // check if values are correct
+        if (time>0 & (unit.equals("sec") | unit.equals("min"))){
+
+            int time_unit = covertTimeUnit(unit);
+            int intGPS = covertGps(gps);
+
+            SQLiteDatabase db = this.getWritableDatabase(); //open database
+            ContentValues values = new ContentValues();
+
+            values.put(Samp_conf_name, name);
+            values.put(Samp_conf_type, type);
+            values.put(Samp_conf_time, time);
+            values.put(Samp_conf_time_unit, time_unit);
+            values.put(Samp_conf_window, window);
+            values.put(Samp_conf_gps, intGPS);
+            values.put(Samp_conf_date, getDateTime());
+
+            db.update(Samp_conf_table, values,"id = ? ", new String[] { Integer.toString(id) } );
+            db.close();
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // UPDATE BY NAME
+    public boolean updateConfigurationByName(String name, String type, int time, String unit, int window, boolean gps) {
+        //TODO on upper level: check if window is greater than sampling time for streaming sensors
+        // check if values are correct
+        if (time>0 & (unit.equals("sec") | unit.equals("min"))){
+
+            int time_unit = covertTimeUnit(unit);
+            int intGPS = covertGps(gps);
+
+            SQLiteDatabase db = this.getWritableDatabase(); //open database
+            ContentValues values = new ContentValues();
+
+            values.put(Samp_conf_name, name);
+            values.put(Samp_conf_type, type);
+            values.put(Samp_conf_time, time);
+            values.put(Samp_conf_time_unit, time_unit);
+            values.put(Samp_conf_window, window);
+            values.put(Samp_conf_gps, intGPS);
+            values.put(Samp_conf_date, getDateTime());
+
+            db.update(Samp_conf_table, values,Samp_conf_name+" = ? ", new String[] {name} );
+            db.close();
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // DELETE BY ID
+    public int deleteConfigurationById(int id){
+        SQLiteDatabase db = this.getWritableDatabase();
+        int del = db.delete(Samp_conf_table,"id = ?",new String[] { Integer.toString(id) });
+        db.close();
+        return del;
+    }
+
+    // DELETE BY ID
+    public int deleteConfigurationByName(String Name){
+        SQLiteDatabase db = this.getWritableDatabase();
+        int del = db.delete(Samp_conf_table,Samp_conf_name+" = ?",new String[] {Name});
+        db.close();
+        return del;
+    }
+
+    public ArrayList getAllConfigurationsWithoutOrder(){
+        ArrayList array_list = new ArrayList();
+        //hp = new HashMap();
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor res =  db.rawQuery( "select * from "+Samp_conf_table, null);
+        db.close();
+        res.moveToFirst();
+        while(!res.isAfterLast()){
+            array_list.add(res.getString(res.getColumnIndex(Samp_conf_name)));
+            res.moveToNext();
+        }
+        return array_list;
+    }
+
+    public ArrayList getAllConfigurationsOrderedByName(){
+        ArrayList array_list = new ArrayList();
+        //hp = new HashMap();
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor res =  db.rawQuery( "select * from "+Samp_conf_table+" order by "+Samp_conf_name+" asc", null);
+        db.close();
+        res.moveToFirst();
+        while(!res.isAfterLast()){
+            array_list.add(res.getString(res.getColumnIndex(Samp_conf_name)));
+            res.moveToNext();
+        }
+        return array_list;
+    }
+
+    public ArrayList getAllConfigurationsOrderedByType(){
+        ArrayList array_list = new ArrayList();
+        //hp = new HashMap();
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor res =  db.rawQuery( "select * from "+Samp_conf_table+" order by "+Samp_conf_type+" asc", null);
+        db.close();
+        res.moveToFirst();
+        while(!res.isAfterLast()){
+            array_list.add(res.getString(res.getColumnIndex(Samp_conf_name)));
+            res.moveToNext();
+        }
+        return array_list;
+    }
+
+    public ArrayList getAllConfigurationsOrderedByTypeThenName(){
+        ArrayList array_list = new ArrayList();
+        //hp = new HashMap();
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor res =  db.rawQuery( "select * from "+Samp_conf_table+" order by "+
+                Samp_conf_type+" asc, "+Samp_conf_name+" asc", null);
+        db.close();
+        res.moveToFirst();
+        while(!res.isAfterLast()){
+            array_list.add(res.getString(res.getColumnIndex(Samp_conf_name)));
+            res.moveToNext();
+        }
+        return array_list;
+    }
+
+    public int numberOfConfigurations(){
+        SQLiteDatabase db = this.getReadableDatabase();
+        int num = (int) DatabaseUtils.queryNumEntries(db, Samp_conf_table);
+        db.close();
+        return num;
+    }
+
+    private String getDateTime() {
+        SimpleDateFormat dateFormat = new SimpleDateFormat(
+                "yyyy-MM-dd HH:mm:ss", Locale.getDefault());
+        Date date = new Date();
+        return dateFormat.format(date);
+    }
+
+    private int covertTimeUnit (String unit) {
+        int time_unit = 0; //default seconds
+        if (unit.equals("min")) {
+            time_unit = 1;
+        }
+        return time_unit;
+    }
+
+    private int covertGps (boolean gps) {
+        int intGPS = 0; //default false
+        if (gps) {
+            intGPS = 1;
+        }
+        return intGPS;
+    }
+
+}
