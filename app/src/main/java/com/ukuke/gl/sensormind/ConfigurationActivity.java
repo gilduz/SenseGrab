bd1f5652ce39eb8c1ddca60e03eadc2792602c61
leov85 <leonardo.vivarelli85@gmail.com>
2015-01-24 18:04:39 +0100

    Sistemati bug nel layout di configuration activity

--- app/src/main/java/com/ukuke/gl/sensormind/ConfigurationActivity.java
+++ app/src/main/java/com/ukuke/gl/sensormind/ConfigurationActivity.java
@@ -1,65 +1,91 @@
 package com.ukuke.gl.sensormind;
 
 import android.app.Activity;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.RadioGroup;
 import android.widget.SeekBar;
+import android.widget.Switch;
 import android.widget.TextView;
 
-import com.ukuke.gl.sensormind.R;
-
 public class ConfigurationActivity extends Activity {
+
+    // Database
+    DbHelper db;
+
+    // Dynamic views in activity_configuration.xml
+    EditText confName; //Name
+    SeekBar seekSamp; //sampling time
+    SeekBar seekWin; //sampling window
+    TextView textSamp;
+    TextView textWin;
+    RadioGroup radioGr;
+    Switch gpsSwitch;
+
+    // Initialize and set default value for seekBars
+    int progressSamp = 5;
+    int progressWin = 5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_configuration);
 
-        SeekBar seekSamp = (SeekBar) findViewById(R.id.Conf_Sam_seekBar);
-        final TextView textSamp = (TextView) findViewById(R.id.Conf_viewSeekbarValue);
+
+
+        // seek bars and relative value views
+        seekSamp = (SeekBar) findViewById(R.id.Conf_Sam_seekBar);
+        textSamp = (TextView) findViewById(R.id.Conf_viewSeekbarValue);
+        seekWin = (SeekBar) findViewById(R.id.Conf_Win_seekBar);
+        textWin = (TextView) findViewById(R.id.Conf_viewWinSeekbarValue);
+
+        // Name
+        confName = (EditText) findViewById(R.id.Conf_InsertName);
+
+        // Radio buttons
+        radioGr = (RadioGroup) findViewById(R.id.Conf_radioGroup);
+
+        // Gps switch
+        gpsSwitch = (Switch) findViewById(R.id.Conf_gps);
+
 
         seekSamp.setMax(60);
+        seekWin.setMax(60);
 
+        // Set values from seekBars
         seekSamp.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 textSamp.setText(String.valueOf(progress));
+                progressSamp = progress;
             }
 
             @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-
-            }
+            public void onStartTrackingTouch(SeekBar seekBar) {}
 
             @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-
-            }
+            public void onStopTrackingTouch(SeekBar seekBar) {}
         });
-
-        SeekBar seekWin = (SeekBar) findViewById(R.id.Conf_Win_seekBar);
-        final TextView textWin = (TextView) findViewById(R.id.Conf_viewWinSeekbarValue);
-
-        seekWin.setMax(60);
 
         seekWin.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 textWin.setText(String.valueOf(progress));
+                progressWin = progress;
             }
 
             @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-
-            }
+            public void onStartTrackingTouch(SeekBar seekBar) {}
 
             @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-
-            }
+            public void onStopTrackingTouch(SeekBar seekBar) {}
         });
+
+        //TODO Aggiungere condizione getExtras per differenziare se si arriva da add device o da main
+        //TODO se si arriva da add device prendere il tipo sensore
+        //TODO se si arriva da main è una modifica, cambiare testo save in update
     }
 
 
@@ -84,4 +110,10 @@ public class ConfigurationActivity extends Activity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    //TODO aggiungere metodo saveOrUpdate() che differenzia a seconda di getextras (persare se settare una variabile invece che usare 2 volte getextras
+    //TODO implementare scrittura o modifica su stringa database
+
+    //TODO aggiungere metodo delete con alert per confermare, implementare cancellazione riga db
+
 }
